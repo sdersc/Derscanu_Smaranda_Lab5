6731ce9f35fd98f6676f49269939fbd04ff8797e
sdersc <43818186+sdersc@users.noreply.github.com>
2020-11-07 19:20:58 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Derscanu_Smaranda_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public object ExpenseDTO { get; internal set; }
+    }
+}
